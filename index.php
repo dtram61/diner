ac1e5df4c14cce9ba8d0c4d82d7f01ec7b839aaa
user.email <dtramgre@srv604.webhostingforstudents.com>
2022-04-26 12:07:35 -0400

    created a order form

--- index.php
+++ index.php
@@ -10,16 +10,36 @@ require_once('vendor/autoload.php');
 //Create an instance of the base class
 $f3 = Base::instance();
 
+// Define a default route
+$f3->route('GET /', function() {
+    //echo "Diner Project";
 
-//Define a default route
-    $f3->route('GET /', function() {
-      // echo "Diner project";
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+}
+);
+
+
+//Define a breakfast route
+    $f3->route('GET /breakfast', function() {
+      //echo "Breakfast project";
 
         $view = new Template();
-        echo $view->render('views/home.html');
+        echo $view->render('views/breakfast.html');
 
     }
     );
+
+//Define a order route
+$f3->route('GET /order', function() {
+    //echo "Breakfast project";
+
+    $view = new Template();
+    echo $view->render('views/orderForm1.html');
+
+}
+);
 
     //Run fat free
 $f3->run();
